69cfaf2656e8d11321af233c34f2f3244c8f4ccc
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-10-07 15:35:48 +0200

    template users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": "Henk de Hopper",
+            "email": "henkdehopper@gmail.com",
+            "role": "ENGINEER"
+        },
+        {
+            "id": 2,
+            "name": "Mara Machtig",
+            "email": "maramachtig@gmail.com",
+            "role": "ADMIN"
+        },
+        {
+            "id": 3,
+            "name": "Kees Kerfstok",
+            "email": "keeskerfstok@gmail.com",
+            "role": "ENGINEER"
+        },
+        {
+            "id": 4,
+            "name": "John Doe",
+            "email": "john.doe@gmail.com",
+            "role": "INTERN"
+        },
+        {
+            "id": 5,
+            "name": "Jane Doe",
+            "email": "jane.doe@gmail.com",
+            "role": "INTERN"
+        }
+    ]
+}
