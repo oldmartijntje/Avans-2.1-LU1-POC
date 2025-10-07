02978493a5b3e26ef1be9b03815b4165935153c1
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-10-07 16:29:26 +0200

    validation decoraters

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateUserDto {
+    // all validation decorators can be found here: https://github.com/typestack/class-validator#validation-decorators
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], {
+        message: "Valid role required"
+    })
     role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
