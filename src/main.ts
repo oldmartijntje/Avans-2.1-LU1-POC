1601c571df60096c95e58afe885e31a002d65ef4
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-10-08 11:18:14 +0200

    loggin the port

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT ?? 6969);
+    console.log(`Server running on http://127.0.0.1:${process.env.PORT ?? 6969}`)
 }
 bootstrap();
