23d64047f3f3e8d555c155fce6becee48de4318b
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-10-16 18:18:05 +0200

    31. listening on not just localhost

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? (() => { throw new Error("PORT is not set in the .env\nHave you used the command: `npm run setup`?\n"); })());
-    console.log(`Server running on http://127.0.0.1:${process.env.PORT ?? 6969}`)
+    const port = process.env.PORT ?? (() => {
+        throw new Error("PORT is not set in the .env\nHave you used the command: `npm run setup`?\n");
+    })();
+
+    // Bind to all interfaces
+    await app.listen(port, '0.0.0.0');
+
+    console.log(`Server running on http://0.0.0.0:${port}`);
 }
 bootstrap();
